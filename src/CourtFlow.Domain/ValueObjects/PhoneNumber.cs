a8bbb7dbeb15bf54ba704dad020c843343fd6ead
OWAIS <120783442+owais-alhjri@users.noreply.github.com>
2026-04-11 19:51:38 +0400

    I added password haser

--- src/CourtFlow.Domain/ValueObjects/PhoneNumber.cs
+++ src/CourtFlow.Domain/ValueObjects/PhoneNumber.cs
@@ -4,14 +4,15 @@ namespace CourtFlow.Domain.ValueObjects;
 
 public record PhoneNumber
 {
-    public string Value { get; }
+    public string Value { get; } 
 
     public PhoneNumber(string value)
     {
+
         if (string.IsNullOrWhiteSpace(value))
             throw new ArgumentException("Phone number cannot be empty.");
 
-        var isValidPhone = Regex.IsMatch(value ?? "", @"^(\+968)?[279]\d{7}$");
+        var isValidPhone = Regex.IsMatch(value, @"^(\+968)?[279]\d{7}$");
         if (!isValidPhone)
             throw new ArgumentException("Invalid Oman phone number.");
         Value = value;
---
+++ src/CourtFlow.Infrastructure/Repositories/BcryptPasswordHasher.cs
@@ -0,0 +1,12 @@
+﻿using CourtFlow.Application.Interfaces;
+
+namespace CourtFlow.Infrastructure.Repositories;
+
+public class BcryptPasswordHasher : IPasswordHasher
+{
+    public string Hash(string password) 
+        => BCrypt.Net.BCrypt.HashPassword(password, workFactor:12);
+
+    public bool Verify(string password, string hash)
+        => BCrypt.Net.BCrypt.Verify(password, hash);
+}
